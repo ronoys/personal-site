c662db7a771e7fd20dcb0600befdf7412916c3ea
ronoy <ronoysarkar.mail@gmail.com>
2019-07-18 21:51:20 -0400

    tile format

---
+++ src/components/ProjectsTiles.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    //minWidth: 275,
+    width: 300,
+    
+  },
+  
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+
+  main: {
+    position:'absolute',
+    width:'1200px',
+    height:'240px',
+    border:'1px solid black',
+    padding:'50px',
+    float: 'right',    
+
+  },
+
+  tile : {
+    padding: '200px',
+    
+  },
+
+    
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+  
+  return (
+    <div id="main">
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+                <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+                <Typography variant="h5" component="h2">
+                Project1
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                
+                </Typography>
+                </CardContent>
+            
+                 </Card>
+
+        </div>
+
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+
+                <CardContent>
+
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                Project2
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                </Typography>
+
+                </CardContent>
+            
+                </Card>
+
+        </div>
+
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+
+                <CardContent>
+
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                Project2
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                </Typography>
+
+                </CardContent>
+            
+                </Card>
+
+        </div>
+
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+
+                <CardContent>
+
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                Project2
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                </Typography>
+
+                </CardContent>
+            
+                </Card>
+
+        </div>
+
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+
+                <CardContent>
+
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                Project2
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                </Typography>
+
+                </CardContent>
+            
+                </Card>
+
+        </div>
+
+        <div id="tile" style={{display: 'inline-block', padding: '20px'}}>
+
+                <Card className={classes.card} style={{height: '250px', padding: '10px' }}>
+
+                <CardContent>
+
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Project
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                Project2
+                </Typography>
+                
+                <Typography variant="body2" component="p">
+                Text for project
+                </Typography>
+
+                </CardContent>
+            
+                </Card>
+
+        </div>
+
+
+
+    
+    </div>
+  );
+}
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,9 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Menu from "../components/menu";
-
 import Button from "../components/Button";
-import Dash from "../components/Dash";
+import ProjectsTiles from "../components/ProjectsTiles"
 
 
 const ThirdPage = () => (
@@ -16,15 +14,11 @@ const ThirdPage = () => (
     <h1>Projects</h1>
     <p>This is a paragraph.</p> 
     </div>
-    
-    <div>
-
+       
     <Button></Button> 
-    <Dash></Dash>
-    
 
-    </div>
-    
+    <ProjectsTiles></ProjectsTiles>
+      
   </Layout>
 )
 
